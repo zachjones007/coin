c6c55569d0b10fd3e20675ddd94dd5fcdf8a5bc4
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-11 09:25:17 -0700

    Update coinfork.1.py

--- coinfork.1.py
+++ coinfork.1.py
@@ -1,36 +1,34 @@
-how could you add can use the SentimentIntensityAnalyzer to analyze the sentiment of news articles or statements made by the Federal Reserve to this code ? 
 import yfinance as yf
 import pandas as pd
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+# download the required resource vader_lexicon
+nltk.download('vader_lexicon')
+
 # create SentimentIntensityAnalyzer object
 sia = SentimentIntensityAnalyzer()
-
-# example statement made by the Federal Reserve
-statement = "The Federal Reserve is committed to maintaining price stability and supporting the economic recovery."
-
-# analyze the sentiment of the statement
-scores = sia.polarity_scores(statement)
-
-# print the sentiment scores
-print(scores)
 
 # get historical data for Fantom
 ftm = yf.Ticker("FTM-USD")
 data = ftm.history(period="max")
 
+# analyze the sentiment of news articles or statements made by the Federal Reserve
+# you'll likely want to use an API or web scraping to obtain the actual text of the articles or statements in your actual implementation
+statements = ["The Federal Reserve is committed to maintaining price stability and supporting the economic recovery.", "The Federal Reserve is concerned about rising inflation and may raise interest rates."]
+scores = [sia.polarity_scores(statement) for statement in statements]
+print(scores)
+
 # calculate the short and long moving averages
 short_moving_average = data['Close'].rolling(window=5).mean()
 long_moving_average = data['Close'].rolling(window=20).mean()
 
-# create a new dataframe with the moving averages
-data = pd.DataFrame({'Close': data['Close'], 'short_moving_average': short_moving_average, 'long_moving_average': long_moving_average})
-
+# create a new dataframe with the moving averages and sentiment scores
+data = pd.DataFrame({'Close': data['Close'], 'short_moving_average': short_moving_average, 'long_moving_average': long_moving_average, 'sentiment': scores})
 # determine the trading signals
 data['signal'] = None
-data.loc[(data['short_moving_average'] > data['long_moving_average']) & (data['short_moving_average'].shift(1) < data['long_moving_average'].shift(1)), 'signal'] = 'buy'
-data.loc[(data['short_moving_average'] < data['long_moving_average']) & (data['short_moving_average'].shift(1) > data['long_moving_average'].shift(1)), 'signal'] = 'sell'
+data.loc[(data['short_moving_average'] > data['long_moving_average']) & (data['short_moving_average'].shift(1) < data['long_moving_average'].shift(1)) & (data['sentiment'] > 0.5), 'signal'] = 'buy'
+data.loc[(data['short_moving_average'] < data['long_moving_average']) & (data['short_moving_average'].shift(1) > data['long_moving_average'].shift(1)) & (data['sentiment'] < 0.5), 'signal'] = 'sell'
 
 # initialize trade statistics
 wins = 0
@@ -42,7 +40,10 @@ for i, row in data.iterrows():
         data.loc[i, 'entry_price'] = row['Close']
     elif row['signal'] == 'sell':
         data.loc[i, 'exit_price'] = row['Close']
-        wins += 1
+        if (data.loc[i, 'exit_price'] - data.loc[i, 'entry_price']) > 0:
+            wins += 1
+        else:
+            losses += 1
 
 # calculate the profit/loss
 data['profit_loss'] = (data['exit_price'] - data['entry_price']) / data['entry_price']
