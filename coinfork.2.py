767dbd7c5d5e36076b5f93199fe06b7f36568c56
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-11 07:20:32 -0700

    Update coinfork.2.py

--- coinfork.2.py
+++ coinfork.2.py
@@ -1,110 +1,63 @@
-help me fix start_date = datetime.datetime(2023, 1, 1)
-end_date = datetime.datetime(2024, 1, 1)
-import yfinance as yf
-import backtrader as bt
+import datetime
 import ccxt
 import pandas as pd
+import backtrader as bt
 
-# create an instance of the exchange object
-exchange = ccxt.binance()
+#create an instance of the exchange object
+exchange = ccxt.binance({'rateLimit': 3000, 'enableRateLimit': True})
 
-# define the symbol and timeframe
+#define the symbol and timeframe
 symbol = 'BTC/USDT'
-timeframe = '1d'
+timeframe = '1m'
 
-# download the historical ohlcv bars
+#download the historical ohlcv bars
 ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
+ohlcv = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+ohlcv.set_index('timestamp', inplace=True)
+ohlcv = ohlcv.resample('15T').agg({'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last', 'volume': 'sum'})
 
-# convert the data to a pandas DataFrame
-data = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-
-# set the timestamp as the DataFrame's index
-data.set_index('timestamp', inplace=True)
-
-# Create a subclass of the Backtrader Strategy class
 class MovingAverageCrossOver(bt.Strategy):
-    params = (
-        ('fast_period', 5),
-        ('slow_period', 20),
-        ('stop_loss_percent', 0.97),
-        ('take_profit_percent', 1.03),
-        ('stop_loss_trailing', True),
-        ('take_profit_trailing', True),
-        ('size', 1),
-        ('commision_percent', 0.01),
-        ('slippage_percent', 0.01)
-    )
+params = (
+('fast_period', 5),
+('slow_period', 20)
+)
+def init(self):
+self.fast_ma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.fast_period)
+self.slow_ma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.slow_period)
 
-    def __init__(self):
-        self.fast_ma = bt.indicators.SimpleMovingAverage(
-            self.data.close, period=self.params.fast_period)
-        self.slow_ma = bt.indicators.SimpleMovingAverage(
-            self.data.close, period=self.params.slow_period)
-        self.stop_loss = None
-        self.take_profit = None
 
-    def next(self):
-        position = self.position.size
-        if self.fast_ma > self.slow_ma:
-            if position < 0:
-                self.close()
-            elif position == 0:
-                size = int(self.params.size * (1 - self.params.commision_percent))
-                self.stop_loss = self.data.close * self.params.stop_loss_percent
-                self.take_profit = self.data.close * self.params.take_profit_percent
-                self.buy(size=size, exectype=bt.Order.Stop, price=self.stop_loss)
-
-        elif self.fast_ma < self.slow_ma:
-            if position > 0:
-                self.close()
-            elif position == 0:
-                size = int(self.params.size * (1 - self.params.commision_percent))
-                
-    def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
-        dt = dt or self.data.datetime[0]
-        if isinstance(dt, float):
-            dt = bt.num2date(dt)
-        print('%s, %s' % (dt.isoformat(), txt))
-
-def notify_order(self, order):
-    if order.status in [order.Submitted, order.Accepted]:
-        return
-    if order.status in [order.Completed]:
-        if order.isbuy():
-            self.log('BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-            (order.executed.price,
-            order.executed.value,
-            order.executed.comm))
-        elif order.issell():
-            self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-            (order.executed.price,
-            order.executed.value,
-            order.executed.comm))
-    elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-        self.log('Order Canceled/Margin/Rejected')
-
-# Create an instance of the cerebro engine
+def next(self):
+    position = self.position.size
+    if self.fast_ma > self.slow_ma and position <= 0:
+        self.buy()
+    elif self.fast_ma < self.slow_ma and position >= 0:
+        self.close()
+#Create an instance of cerebro
 cerebro = bt.Cerebro()
 
-# Add the strategy to cerebro
-cerebro.addstrategy(MovingAverageCrossOver)
+#Create a Backtrader data feed from the pandas DataFrame
+data = bt.feeds.PandasData(dataname=ohlcv)
 
-# Create a Backtrader data feed from the pandas DataFrame
-data = bt.feeds.PandasData(dataname=data)
-
-# Add the data feed to cerebro
+#Add the data feed to cerebro
 cerebro.adddata(data)
 
-# Set the start and end date of the strategy
-start_date = datetime.datetime(2023, 1, 1)
-end_date = datetime.datetime(2024, 1, 1)
-cerebro.broker.set_cash(100000)
+#Add the strategy to cerebro
+cerebro.addstrategy(MovingAverageCrossOver)
 
-# Run the strategy
+cerebro.broker.setcash(100000.0)
+
+#Set the commission - 0.1% ... divide by 100 to remove the %
+cerebro.broker.setcommission(commission=0.001)
+
+#Print out the starting conditions
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+#Run the strategy
 cerebro.run()
 
-print(data)
+#Print out the final result
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
 
 
 
