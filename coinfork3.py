8117c3e2449200f001e20027e7c72454fc0c97f3
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-11 07:37:46 -0700

    Update and rename coinfork3 to coinfork3.py

--- coinfork3.py
+++ coinfork3.py
@@ -60,3 +60,22 @@ for i, row in data.iterrows():
     elif row['signal'] == 'sell':
         data.loc[i, 'exit_price'] = row['Close']
     elif row['Close'] > data.loc[i, 'stop_loss']:
+        
+# iterate over the buy trades and calculate the profit/loss
+for i, row in buy_data.iterrows():
+    exit_index = sell_data[sell_data.index > i].index[0]
+    data.loc[exit_index, 'profit_loss'] = (data.loc[exit_index, 'exit_price'] - row['entry_price'])/row['entry_price']
+
+# select the last 3 buy trades
+last_3_buy_trades = buy_data.tail(3)
+
+# iterate over the last 3 buy trades and print the information
+for i, row in last_3_buy_trades.iterrows():
+    print(f'Trade: {i}, Entry Price: {row["entry_price"]}, Exit Price: {data.loc[row.name, "exit_price"]}, Profit/Loss: {data.loc[row.name, "profit_loss"]}')
+
+# select the last 3 sell trades
+last_3_sell_trades = sell_data.tail(3)
+
+# iterate over the last 3 sell trades and print the information
+for i, row in last_3_sell_trades.iterrows():
+    print(f'Trade: {i}, Entry Price: {data.loc[row.name, "entry_price"]}, Exit Price: {row["exit_price"]}, Profit/Loss: {data.loc[row.name, "profit_loss"]}')
